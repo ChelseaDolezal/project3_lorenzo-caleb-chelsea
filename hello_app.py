90c72ac34b620fe1d54a44a62f9b99c9a6352585
Lorenzo Davis <lorenzodavis@Lorenzos-Laptop.local>
2024-07-11 21:26:51 -0500

    updating columns

--- hello_app.py
+++ hello_app.py
@@ -18,18 +18,35 @@ def get_accidents():
     accidents_list = []
     for accident in accidents:
         accident_data = {
-            'Start_Time': accident['Start_Time'],
-            'End_Time': accident['End_Time'],
-            'Severity': accident['Severity'],
-            'Latitude': accident['Latitude'],
-            'Longitude': accident['Longitude'],
-            'Description': accident['Description'],
-            'City': accident['City'],
-            'State': accident['State'],
-            'Temperature': accident['Temperature'],
-            'Visibility': accident['Visibility'],
-            'Weather_Condition': accident['Weather_Condition'],
-            'Wind_Speed': accident['Wind_Speed']
+            'State': accident.get('STATE'),
+            'StateName': accident.get('STATENAME'),
+            'CaseID': accident.get('ST_CASE'),
+            'Pedestrians': accident.get('PEDS'),
+            'PersonsNotInMotorVehicle': accident.get('PERNOTMVIT'),
+            'TotalVehicles': accident.get('VE_TOTAL'),
+            'VehicleForms': accident.get('VE_FORMS'),
+            'PersonsInvolved': accident.get('PERSONS'),
+            'PersonsKilled': accident.get('PERMVIT'),
+            'City': accident.get('CITY'),
+            'CityName': accident.get('CITYNAME'),
+            'Month': accident.get('MONTH'),
+            'MonthName': accident.get('MONTHNAME'),
+            'Day': accident.get('DAY'),
+            'DayName': accident.get('DAYNAME'),
+            'DayOfWeek': accident.get('DAY_WEEK'),
+            'DayOfWeekName': accident.get('DAY_WEEKNAME'),
+            'Year': accident.get('YEAR'),
+            'Hour': accident.get('HOUR'),
+            'HourName': accident.get('HOURNAME'),
+            'Minute': accident.get('MINUTE'),
+            'MinuteName': accident.get('MINUTENAME'),
+            'Latitude': accident.get('LATITUDE'),
+            'Longitude': accident.get('LONGITUD'),
+            'HarmfulEvent': accident.get('HARM_EV'),
+            'HarmfulEventName': accident.get('HARM_EVNAME'),
+            'Weather': accident.get('WEATHER'),
+            'WeatherName': accident.get('WEATHERNAME'),
+            'Fatalities': accident.get('FATALS')
         }
         accidents_list.append(accident_data)
     return jsonify(accidents_list)
